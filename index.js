35661309fade7eaeb1cb5227ffd3cb9a581377b3
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-10-22 12:15:18 +0300

    add authorizations

--- index.js
+++ index.js
@@ -14,19 +14,24 @@ var resources = {
     'charts': '/charts/popular',
     'genres': '/genres',
     'genre': '/genres/{genre}',
-    'genre/sites': '/genres/{genre}/sites',
+    'genre-sites': '/genres/{genre}/sites',
     'sites': '/sites',
     'site': '/sites/{id}',
     'tracks': '/tracks',
     'track': '/tracks/{id}'
 };
 
-function Shuffler (appKey) {
+function Shuffler (appKey, appSecret) {
     if (!appKey || typeof appKey !== 'string') {
-        throw new Error('AppKey string is required - http://developers.shuffler.fm/#app_key');
+        throw new Error('App Key is required - http://developers.shuffler.fm/#app_key');
+    }
+    if (!appSecret || typeof appSecret !== 'string') {
+        throw new Error('App Secret is required - http://developers.shuffler.fm/#app_key');
     }
     this.appKey = appKey;
+    this.appSecret = appSecret;
     this.baseUrl = 'https://api.shuffler.fm/v2';
+    this.authBaseUrl = 'https://shuffler.fm/authorizations';
 }
 
 Shuffler.prototype = {
@@ -34,8 +39,34 @@ Shuffler.prototype = {
         return this.appKey;
     },
 
+    getAppSecret: function () {
+        return this.appSecret;
+    },
+
     getBaseUrl: function () {
         return this.baseUrl;
+    },
+
+    getAuthorizationsBaseUrl: function () {
+        return this.authUrl;
+    },
+
+    createAuthUrl: function (scope, redir_uri) {
+        if (!scope || !scope.length) {
+            throw new Error('Define scope for auth - http://developers.shuffler.fm/#authorizations');
+        }
+        if (!redir_uri || typeof redir_uri !== 'string') {
+            throw new Error('Redirect uri is required - http://developers.shuffler.fm/#authorizations');
+        }
+        scopes = typeof scopes === 'string' ? scopes : scopes.join(',');
+        return this.getAuthorizationsBaseUrl() + '?app_key=' + this.getAppKey() + '&scope=' + scope + '&redir_uri=' + redir_uri;
+    },
+
+    getAuthToken: function (code, callback) {
+        var url = this.getAuthorizationsBaseUrl() + '?app_key=' + this.getAppKey() + '&app_secret=' + this.getAppSecret();
+        return request({method: 'POST', url: url, body: {code: code}}, function (err, response, body) {
+
+        });
     },
 
     _createUrl: function (alias, params, qs) {
@@ -58,7 +89,7 @@ Shuffler.prototype = {
         return this.getBaseUrl() + url + '?app-key=' + this.getAppKey() + (qs ? '&'+qs : '');
     },
 
-    _request: function (method, url, callback) {
+    _get: function (url, callback) {
         return request({method: method, url: url}, function (err, response, body) {
             if (err) {
                 return callback(err);
@@ -78,7 +109,7 @@ Shuffler.prototype = {
 
     getActivityById: function (id, callback) {
         var url = this._createUrl('activity', {id: id});
-        this._request('GET', url, callback);
+        this._get(url, callback);
     },
 
     getArtists: function (query, callback) {
@@ -88,32 +119,32 @@ Shuffler.prototype = {
         }
 
         var url = this._createUrl('artists', {}, (query ? {q: query} : {}));
-        this._request('GET', url, callback);
+        this._get(url, callback);
     },
 
     getArtistById: function (id, callback) {
         var url = this._createUrl('artist', {id: id});
-        this._request('GET', url, callback);
+        this._get(url, callback);
     },
 
     getGenres: function (genre, callback) {
         var url = this._createUrl('genres', {genre: genre});
-        this._request('GET', url, callback);
+        this._get(url, callback);
     },
 
     getGenreByName: function (genre, callback) {
         var url = this._createUrl('genre', {genre: genre});
-        this._request('GET', url, callback);
+        this._get(url, callback);
     },
 
     getGenreSites: function (genre, callback) {
-        var url = this._createUrl('genre/sites', {genre: genre});
-        this._request('GET', url, callback);
+        var url = this._createUrl('genre-sites', {genre: genre});
+        this._get(url, callback);
     },
 
     getChartsPopular: function (callback) {
         var url = this._createUrl('charts');
-        this._request('GET', url, callback);
+        this._get(url, callback);
     },
 
     getChannel: function (channel, position, callback) {
@@ -124,17 +155,17 @@ Shuffler.prototype = {
 
         var qs = position ? {position: position} : {};
         var url = this._createUrl('channel', {channel: channel}, qs);
-        this._request('GET', url, callback);
+        this._get(url, callback);
     },
 
     getTracks: function (callback) {
         var url = this._createUrl('tracks');
-        this._request('GET', url, callback);
+        this._get(url, callback);
     },
 
     getTrackById: function (id, callback) {
         var url = this._createUrl('track', {id: id});
-        this._request('GET', url, callback);
+        this._get(url, callback);
     },
 
     getSites: function (query, callback) {
@@ -144,17 +175,17 @@ Shuffler.prototype = {
         }
 
         var url = this._createUrl('sites', {}, (query ? {q: query} : {}));
-        this._request('GET', url, callback);
+        this._get(url, callback);
     },
 
     getSiteById: function (id, callback) {
         var url = this._createUrl('site', {id: id});
-        this._request('GET', url, callback);
+        this._get(url, callback);
     },
 
     getFeaturedSites: function () {
         var url = this._createUrl('sites', {}, {filter: 'featured'});
-        this._request('GET', url, callback);
+        this._get(url, callback);
     }
 };
 
